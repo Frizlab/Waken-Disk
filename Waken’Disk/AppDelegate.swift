00603a666a6b2d624710bbbe810be3fe7dccf144
Frizlab <fload@me.com>
2014-07-28 14:42:22 +0200

    Initial Commit

---
+++ Waken’Disk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Waken’Disk
+//
+//  Created by François LAMBOLEY on 28/07/14.
+//  Copyright (c) 2014 Frizlab. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Waken’Disk/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Waken’Disk
+//
+//  Created by François LAMBOLEY on 28/07/14.
+//  Copyright (c) 2014 Frizlab. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
